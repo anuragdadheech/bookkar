1138fd31a351a62a1e345557b2cbaf0aedb6f08d
Anurag Dadheech <anuragdadheech1990@gmail.com>
2014-08-04 20:09:17 +0530

    Updated login main and sign up screen

---
+++ src/com/asa/bookkar/SignUpActivity.java
@@ -0,0 +1,11 @@
+package com.asa.bookkar;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SignUpActivity extends Activity {
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_screen);
+    }
+}
